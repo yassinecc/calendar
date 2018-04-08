ada483523a5a76cfebadeff044f3ab820e36c266
Yassine Chbani <yassinec@bam.tech>
2018-04-08 20:10:43 +0200

    Change default server route

--- server/server.js
+++ server/server.js
@@ -12,9 +12,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Hello!',
-}));
+app.get('/', (req, res) =>
+  res.status(200).send({
+    message: 'Hello!',
+  })
+);
 
 module.exports = app;
